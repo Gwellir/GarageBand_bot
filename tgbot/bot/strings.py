bd0d638f3d99bf057a3906c1141bad3614626997
Vlad Chekunov <gwellir@gmail.com>
2021-05-22 20:29:48 +0300

    actualize plan and strings

--- tgbot/bot/strings.py
+++ tgbot/bot/strings.py
@@ -58,5 +58,105 @@ stages_info = [
                 },
             ]
         ]
-    }
+    },
+    {  # stage 5
+        'text': 'Расскажите подробнее, чтобы мастер мог оценить объем работы',
+        'buttons': [
+            [
+                {
+                    'text': "Отменить",
+                    'callback_data': 'restart',
+                },
+            ]
+        ]
+    },
+    {  # stage 6
+        'text': 'Вы можете добавить к посту фотографии, так будет понятнее',
+        'buttons': [
+            [
+                {
+                    'text': "Добавить",
+                    'callback_data': 'have_photos',
+                },
+                {
+                    'text': 'Пропустить',
+                    'callback_data': 'skip_photos',
+                },
+            ],
+            [
+                {
+                    'text': "Отменить",
+                    'callback_data': 'restart',
+                },
+            ],
+        ]
+    },
+    {  # stage 7
+        'text': 'Загрузите фото, когда закончите - нажмите "Следующий этап"\n(отправляйте файлы по одному)',
+        'buttons': [
+            [
+                {
+                    'text': "Следующий этап",
+                    'callback_data': 'photos_confirm',
+                },
+                {
+                    'text': "Отменить",
+                    'callback_data': 'restart',
+                },
+            ]
+        ]
+    },
+    {  # stage 8
+        'text': 'В каком городе, районе Вы хотели бы провести ремонт?',
+        'buttons': [
+            [
+                {
+                    'text': "Отменить",
+                    'callback_data': 'restart',
+                },
+            ]
+        ]
+    },
+    {  # stage 9
+        'text': 'Укажите свой номер телефона для связи',
+        'buttons': [
+            [
+                {
+                    'text': "Отменить",
+                    'callback_data': 'restart',
+                },
+            ]
+        ]
+    },
+    {  # stage confirmation
+        'text': 'Вы молодец! Смотрите, что у нас получилось:',
+        'buttons': [],
+    },
+    {  # stage finished
+        'text': 'Благодарим за сотрудничество!\nВаша заявка опубликована.',
+        'buttons': [
+            [
+                {
+                    'text': "Начать заново",
+                    'callback_data': 'restart',
+                },
+            ]
+        ]
+    },
 ]
+
+summary = {
+    'text': 'Задание: %s\n\nПодробнее: %s\n\nТелефон: %s\nTG: %s',
+    'buttons': [
+        [
+            {
+                'text': "Зарегистрировать",
+                'callback_data': 'final_confirm',
+            },
+            {
+                'text': 'Отменить',
+                'callback_data': 'restart',
+            }
+        ]
+    ]
+}
