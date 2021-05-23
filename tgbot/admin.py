cf1acc9ad5b1bb9ff401ed68221e42bff0a634b0
Vlad Chekunov <gwellir@gmail.com>
2021-05-23 15:26:26 +0300

    add tables to admin interface

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import BotUser, RequestPhoto, WorkRequest
+
 # Register your models here.
+admin.site.register(BotUser)
+admin.site.register(RequestPhoto)
+admin.site.register(WorkRequest)
