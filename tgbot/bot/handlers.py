c98e2d9babafa83da020b56ddb06fb534bd9f475
Vlad Chekunov <gwellir@gmail.com>
2021-05-25 18:53:02 +0300

    add a Dialog DB model reorganize event processing convert processor functions highly dependant on Dialog class into class structure prevent irrelevant bot update and context data from being passed around add channel posting functionality

--- tgbot/bot/handlers.py
+++ tgbot/bot/handlers.py
@@ -1,9 +1,8 @@
-from tgbot.bot.dialog import Dialog
+from tgbot.bot.dialog import DialogProcessor
 from tgbot.models import BotUser
 
 
 def message_processor(update, context):
-    user, created = BotUser.get_or_create(update, context)
 
-    dialog = Dialog(user)
+    dialog = DialogProcessor(update)
     dialog.process(update, context)
