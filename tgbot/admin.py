8da62c342ad031a71c0126772857b9b5f033e6f0
Vlad Chekunov <gwellir@gmail.com>
2021-06-03 19:30:09 +0300

    add RegisteredRequest to admin interface

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import BotUser, Dialog, RequestPhoto, WorkRequest
+from .models import BotUser, Dialog, RegisteredRequest, RequestPhoto, WorkRequest
 
-# Register your models here.
+# todo rework admin interface to answer project's needs
 admin.site.register(BotUser)
 admin.site.register(RequestPhoto)
 admin.site.register(WorkRequest)
+admin.site.register(RegisteredRequest)
 admin.site.register(Dialog)
