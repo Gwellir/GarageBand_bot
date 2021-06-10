4103cb2c781af59ad87d65886f6834877f29e9b0
Vlad Chekunov <gwellir@gmail.com>
2021-06-11 00:50:08 +0300

    add log viewer view fix static dirs

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,4 +6,10 @@ app_name = "adminapp"
 
 urlpatterns = [
     path("logs/", adminapp.logs_list_view, name="log_view"),
+    path("logs/<int:user_pk>/", adminapp.logs_list_view, name="log_view_by_user"),
+    path(
+        "logs/<int:user_pk>/<int:dialog_pk>/",
+        adminapp.logs_list_view,
+        name="log_view_by_dialog",
+    ),
 ]
--- garage_band_bot/settings.py
+++ garage_band_bot/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = ""
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
