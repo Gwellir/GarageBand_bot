8d7364ca12060e5cae723a13b831fec0fced9c81
Vlad Chekunov <gwellir@gmail.com>
2021-06-29 14:17:06 +0300

    move models to convoapp register convoapp in settings make custom migrations so the existing data is copied over

--- garage_band_bot/settings.py
+++ garage_band_bot/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tgbot",
     "adminapp",
+    "convoapp",
 ]
 
 MIDDLEWARE = [
--- tgbot/models.py
+++ tgbot/models.py
@@ -4,28 +4,14 @@ import tempfile
 
 from django.core.files import File
 from django.db import models, transaction
-from django.utils.translation import gettext_lazy as _
 
+from convoapp.replies import get_admin_message, get_summary_for_request
 from garage_band_bot.settings import ADMIN_GROUP_ID, PUBLISHING_CHANNEL_NAME
 from logger.log_config import BOT_LOG
 from logger.log_strings import LogStrings
 from tgbot.bot.constants import DEFAULT_LOGO_FILE
-from tgbot.bot.replies import get_admin_message, get_summary_for_request
 from tgbot.bot.senders import publish_summary_return_id, send_message_return_id
 from tgbot.exceptions import UserIsBannedError
-
-
-class DialogStage(models.IntegerChoices):
-    """Набор стадий проведения диалога."""
-
-    WELCOME = 1, _("Приветствие")
-    GET_NAME = 2, _("Получить имя")
-    GET_REQUEST_TAG = 3, _("Получить категорию заявки")
-    GET_CAR_TYPE = 4, _("Получить тип автомобиля")
-    GET_REQUEST_DESC = 5, _("Получить описание заявки")
-    REQUEST_PHOTOS = 6, _("Предложить отправить фотографии")
-    CHECK_DATA = 7, _("Проверить заявку")
-    DONE = 8, _("Работа завершена")
 
 
 # todo merge with Django Auth User
@@ -164,7 +150,7 @@ class WorkRequest(models.Model):
         verbose_name="Флаг отказа от проведения заявки", default=False, db_index=True
     )
     dialog = models.OneToOneField(
-        "Dialog",
+        "convoapp.Dialog",
         on_delete=models.SET_NULL,
         null=True,
         related_name="request",
@@ -316,89 +302,4 @@ class RequestPhoto(models.Model):
     image = models.ImageField(verbose_name="Фотография", upload_to="user_photos")
     request = models.ForeignKey(
         WorkRequest, on_delete=models.CASCADE, related_name="photos", db_index=True
-    )
-
-
-class Dialog(models.Model):
-    """
-    Модель диалога с пользователем
-
-    Содержит связь с пользователем, этап диалога, флаг завершения
-    и данные о времени начала и последнем сообщении пользователя.
-    """
-
-    user = models.ForeignKey(BotUser, on_delete=models.CASCADE, related_name="dialog")
-    stage = models.PositiveSmallIntegerField(
-        verbose_name="Состояние диалога",
-        choices=DialogStage.choices,
-        null=False,
-        default=DialogStage.WELCOME,
-    )
-    is_finished = models.BooleanField(
-        verbose_name="Диалог завершён", default=False, db_index=True
-    )
-    last_active = models.DateTimeField(
-        verbose_name="Время последней активности",
-        auto_now=True,
-    )
-    created_at = models.DateTimeField(
-        verbose_name="Время создания",
-        auto_now_add=True,
-    )
-
-    def __str__(self):
-        return f"{self.pk} {self.user} @{self.stage}"
-
-    @classmethod
-    @transaction.atomic()
-    def get_or_create(cls, update):
-        """
-        Получает из базы, либо создаёт структуру из пользователя, диалога и заявки.
-        Если все связанные с пользователем диалоги завершены - формирует новую пару
-        диалог-заявка.
-        """
-
-        user, u_created = BotUser.get_or_create(update)
-        dialog, d_created = cls.objects.get_or_create(user=user, is_finished=False)
-        request, r_created = WorkRequest.get_or_create(user, dialog)
-
-        # todo implement reloading request drafts
-
-        return dialog
-
-    def finish(self):
-        """
-        Завершает диалог.
-
-        Если связанная заявка не опубликована, ей выставляется флаг is_discarded,
-        в ином случае - is_finished.
-        """
-
-        if not self.request.is_complete:
-            self.request.is_discarded = True
-            self.request.save()
-        self.is_finished = True
-        self.save()
-
-
-class Message(models.Model):
-    """
-    Модель для хранения сообщений из диалога
-
-    Содержи связь с диалогом, содержимое сообщения, стадию диалога на момент
-    формирования, информацию о направлении и времени создания.
-    """
-
-    dialog = models.ForeignKey(
-        Dialog, on_delete=models.CASCADE, related_name="messages", db_index=True
-    )
-    text = models.TextField(verbose_name="Текст сообщения")
-    stage = models.PositiveSmallIntegerField(
-        verbose_name="Стадия диалога", choices=DialogStage.choices
-    )
-    is_incoming = models.BooleanField(
-        verbose_name="Входящее", db_index=True, default=True
-    )
-    created_at = models.DateTimeField(
-        verbose_name="Время создания", auto_now_add=True, db_index=True
     )
