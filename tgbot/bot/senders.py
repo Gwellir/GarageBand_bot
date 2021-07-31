c3e1834b39641ce9510c7e77f5f90c267efc0048
Vlad Chekunov <gwellir@gmail.com>
2021-07-31 17:27:43 +0300

    fix linting

--- tgbot/bot/senders.py
+++ tgbot/bot/senders.py
@@ -47,6 +47,7 @@ def send_messages_return_ids(message_data, user_id, msg_bot, reply_to=None):
             msgs = bot.send_media_group(chat_id=user_id, media=album)
         elif len(album):
             msgs = [bot.send_photo(photo=album[0].media, chat_id=user_id)]
+        time.sleep(1)
         text_msg = bot.send_message(
             text=message_data["text"],
             disable_web_page_preview=True,
